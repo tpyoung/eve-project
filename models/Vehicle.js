f507fbafe2f7802dfbea3dde423f9dc2765d7014
Laura <lg3295@gmail.com>
2016-05-16 19:29:50 -1000

    Sets up schema for vehicle collection

---
+++ models/Vehicle.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require('mongoose'),
+        Schema = mongoose.Schema;
+
+
+const vehicleSchema = mongoose.Schema({
+  country: String,
+  manufacturer: String,
+  model: String,
+  body: String,
+  power: String,
+  msrp: Number,
+  mpgmpge: Number,
+  range: Number,
+  maintenance: Number,
+  comparableManufacturer: String,
+  comparableModel: String,
+});
