adc928dec766f5eb33161a304ee2012765dd3fb4
maliaoreta <maliaoreta@gmail.com>
2016-05-18 21:16:46 -1000

    sets up project structure, not tested yet

--- client/app.js
+++ client/app.js
@@ -4,3 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './ public/store'
+
+// NOTHING WAS TESTED YET
+// JUST COPIES PROJECT STRUCTURE FROM BOILER PLATE
