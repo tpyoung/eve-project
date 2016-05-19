d05407bef0046583e967976134b89f93f316583b
maliaoreta <maliaoreta@gmail.com>
2016-05-19 09:07:04 -1000

    project structure set up and runs

--- client/app.js
+++ client/app.js
@@ -1,9 +1,6 @@
 'use strict';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './ public/store'
+
 
 // NOTHING WAS TESTED YET
 // JUST COPIES PROJECT STRUCTURE FROM BOILER PLATE
