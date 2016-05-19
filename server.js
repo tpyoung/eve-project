db4700e6a5e3008863d81bd83de9f265560b87c8
Laura <lg3295@gmail.com>
2016-05-18 21:01:03 -1000

    WIP adding get request for all the states in the db

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const express = require('express'),
            db = mongoose.connection,
        Schema = mongoose.Schema,
    bodyParser = require('body-parser'),
-        State = require('./models/State.js'),
-      Vehicle = require('./models/Vehicle.js'),
-      stateSeed = require('./seedState.js'),
-      vehicleSeed = require('./seedVehicle.js');
+        states = require('./models/State.js'),
+      Vehicle = require('./models/Vehicle.js');
+    // stateSeed = require('./seedState.js'),
+  // vehicleSeed = require('./seedVehicle.js');
 
 
 app.use(express.static('public'));
@@ -27,6 +27,17 @@ mongoose.connect('mongodb://localhost/eve');
 
 // stateSeed();
 // vehicleSeed();
+
+app.get('/api/states', (req, res) => {
+  console.log(req.body);
+  states.findOne(), (err, states) => {
+  console.log(states);
+    if (err) {
+      return console.log(err)
+    }
+    res.send(states)
+  };
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
