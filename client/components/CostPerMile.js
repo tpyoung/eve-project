fa0f5fecf16950983546709c6534bbdf11971d3f
Laura <lg3295@gmail.com>
2016-05-20 12:20:41 -1000

    data includes energy per 100 miles

---
+++ client/components/CostPerMile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+const CostPerMileGraph = React.createClass({
+  render() {
+    return (
+
+    )
+  }
+})
+
+//Take miles per tank / miles per gallon = gallons
+//Gallons *
