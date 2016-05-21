8e2ed9686c24ba2ac94f8bc243333c0c03a5ebf2
Laura <lg3295@gmail.com>
2016-05-20 16:44:20 -1000

    WIP testing that props come back properly

--- client/components/StateIncentives.js
+++ client/components/StateIncentives.js
@@ -3,10 +3,16 @@ import React from 'react';
 
 const StateIncentives = React.createClass({
   render() {
-  console.log(this.props);
+  if (this.props.stateInfo[0] === undefined && this.props.vehicleInfo[0] === undefined){
+    console.log('nothing');
+  }
+  else {
+    console.log(this.props.stateInfo[0].incentivesHybrid[0], this.props)
+  }
   var grayImgArr = ['gray1', 'gray2', 'gray3'];
   var colorImgArr = ['link', 'link', 'link'];
     if (true) {
+      //GAS
       var imageNode = grayImgArr.map(function(img) {
         console.log(img);
         return (
@@ -17,13 +23,20 @@ const StateIncentives = React.createClass({
         <div className="StateIncentives">
           <h1>Gas Vehicle</h1>
           {imageNode}
+          {Object.keys(this.props.stateInfo[0].incentivesHybrid[0])}
         </div>
       )
     }
     // else if (true) {
-
+    //   //HYBRID
+    //   trueArr = [];
+    //   falseArr = [];
+    //   for key in this.props.stateInfo.incentivesHybrid {
+    //     if (key === false) {
+    //       falseArr.push(key);
+    //     }
+    //   }
     // }
-
   }
 });
 
