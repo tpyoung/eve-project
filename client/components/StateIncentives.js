8b2430bdf45a854ce9d4909f3f51f9c00ee04175
Laura <lg3295@gmail.com>
2016-05-20 16:04:17 -1000

    capitalizes State in StateIncentives file

---
+++ client/components/StateIncentives.js
@@ -0,0 +1,32 @@
+'use strict';
+import React from 'react';
+
+const StateIncentives = React.createClass({
+  render() {
+  console.log(this.props);
+  var grayImgArr = ['gray1', 'gray2', 'gray3'];
+  var colorImgArr = ['link', 'link', 'link'];
+    if (true) {
+      var imageNode = grayImgArr.map(function(img) {
+        console.log(img);
+        return (
+          <p>{img}</p>
+        )
+      })
+      return (
+        <div className="StateIncentives">
+          <h1>Gas Vehicle</h1>
+          {imageNode}
+        </div>
+      )
+    }
+
+  }
+});
+
+export default StateIncentives;
+
+
+//if power === gas
+//if power === hybrid, stateInfo.incentivesHybrid
+//if power === electric, stateInfo.incentivesElectric
