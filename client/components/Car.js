793f61f1462139fd72c59c29a7ba99736ea30d70
maliaoreta <maliaoreta@gmail.com>
2016-05-21 09:20:48 -1000

    fixes toggling left/right for electric vehicles

--- client/components/Car.js
+++ client/components/Car.js
@@ -53,30 +53,32 @@ const Car = React.createClass({
       <div className="cars" style={style}>
         {this.props.gasCars[0] && <div className="gasCar">
           <h3>Gas</h3>
-          {this.props.gasCars[this.state.gasIndex].manufacturer} 
+          {this.props.gasCars[this.state.gasIndex].manufacturer}<br/>
           {this.props.gasCars[this.state.gasIndex].model}<br/>
-          {this.props.gasCars[this.state.gasIndex].msrp}<br/>
-          {this.props.gasCars[this.state.gasIndex].mpgmpge.gas}<br/>
+          MSRP: {this.props.gasCars[this.state.gasIndex].msrp}<br/>
+          MPGMPGE Gas: {this.props.gasCars[this.state.gasIndex].mpgmpge.gas}<br/>
           <button onClick={this.prevGasCar}>left</button>
           <button onClick={this.nextGasCar}>right</button>
         </div>}
         {this.props.hybridCars[0] &&
         <div className="hybridCar">
           <h3>Plug-In Hybrid</h3>
-          {this.props.hybridCars[this.state.hybridIndex].manufacturer} 
+          {this.props.hybridCars[this.state.hybridIndex].manufacturer}<br/> 
           {this.props.hybridCars[this.state.hybridIndex].model}<br/>
-          {this.props.hybridCars[this.state.hybridIndex].msrp}<br/>
-          {this.props.hybridCars[this.state.hybridIndex].mpgmpge.gas}<br/>
-          <button onClick={this.prevElectricCar}>left</button>
-          <button onClick={this.nextElectricCar}>right</button>
+          MSRP: {this.props.hybridCars[this.state.hybridIndex].msrp}<br/>
+          MPGMPGE Electric: {this.props.hybridCars[this.state.hybridIndex].mpgmpge.electric}<br/>
+          MPGMPGE Gas: {this.props.hybridCars[this.state.hybridIndex].mpgmpge.gas}<br/>
+          <button onClick={this.prevHybridCar}>left</button>
+          <button onClick={this.nextHybridCar}>right</button>
         </div>}
-        {this.props.hybridCars[0] &&
+        {this.props.electricCars[0] &&
         <div className="electricCar">
           <h3>Electric</h3>
-          {this.props.electricCars[this.state.electricIndex].manufacturer} 
+          {this.props.electricCars[this.state.electricIndex].manufacturer}<br/> 
           {this.props.electricCars[this.state.electricIndex].model}<br/>
-          {this.props.electricCars[this.state.electricIndex].msrp}<br/>
-          {this.props.electricCars[this.state.electricIndex].mpgmpge.gas}<br/>
+          MSRP: {this.props.electricCars[this.state.electricIndex].msrp}<br/>
+          MPGMPGE Electric: {this.props.electricCars[this.state.electricIndex].mpgmpge.electric}<br/>
+          MPGMPGE Gas: {this.props.electricCars[this.state.electricIndex].mpgmpge.gas}<br/>
           <button onClick={this.prevElectricCar}>left</button>
           <button onClick={this.nextElectricCar}>right</button>
         </div>}
@@ -84,7 +86,6 @@ const Car = React.createClass({
     )
   },
   render: function () {
-    console.log('this.props.electricCars: ', this.props.electricCars);
     return (
       <div>
       {(this.props.gasCars[0] || this.props.hybridCars[0] || this.props.electricCars[0]) && this.getSingleCar()}
