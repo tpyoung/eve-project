04607d739294a4964a544d6edec239ea5c9220c7
Laura <lg3295@gmail.com>
2016-05-20 15:07:31 -1000

    sets up incentives component shell

---
+++ client/components/stateIncentives.js
@@ -0,0 +1,18 @@
+'use strict';
+import React from 'react';
+
+const stateIncentives = React.createClass({
+  render() {
+    if (this.props.vehicle.power )
+
+    return (
+
+    )
+  }
+});
+
+export default stateIncentives;
+
+//array with gray picture links
+//array with color picture links
+
