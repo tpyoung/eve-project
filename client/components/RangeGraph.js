bd585f41de5ba2a813b61610736eedf6fcb6bc11
Laura <lg3295@gmail.com>
2016-05-23 19:17:34 -1000

    sets varying graphs to display depending on vehicle results, lets user know if there is no comparison available

--- client/components/RangeGraph.js
+++ client/components/RangeGraph.js
@@ -14,7 +14,6 @@ const RangeGraph = React.createClass({
       var gasRange = gasVehicle.range.gas;
       var hybridGasRange = hybridVehicle.range.gas;
       var hybridElectricRange = hybridVehicle.range.electric;
-      console.log('hybridGasRange',hybridGasRange);
       var electricRange = electricVehicle.range.electric;
       var chart = c3.generate({
         bindto: '#RangeGraph',
@@ -53,44 +52,108 @@ const RangeGraph = React.createClass({
         }
       }); //end of CHART
     } //end IF
+    else if (this.props.gasVehicle[0] !== undefined &&
+      this.props.electricVehicle[0] !== undefined) {
+      var gasVehicle = this.props.gasVehicle[0];
+      var electricVehicle = this.props.electricVehicle[0];
+      var gasRange = gasVehicle.range.gas;
+      var electricRange = electricVehicle.range.electric;
+      var chart = c3.generate({
+        bindto: '#RangeGraph',
+        data: {
+          columns: [
+              ['Gas', gasRange],
+              ['Electric', electricRange]
+          ],
+          types: {
+              Gas: 'bar',
+              Electric: 'bar'
+          },
+          colors: {
+            Gas: '#4F4F4F',
+            Electric: '#93CC5D'
+          },
+          transition: {
+            duration: 2000
+          }
+        },
+        axis: {
+          rotated: true,
+          x: {show:true},
+          y: {show:true}
+        },
+        tooltip: {
+          format: {
+            title: function () { return 'Vehicle Range (Miles)'; }
+          }
+        }
+      }); //end of CHART
+    } //ELSE IF
+    else if (this.props.gasVehicle[0] !== undefined &&
+      this.props.hybridVehicle[0] !== undefined) {
+      var gasVehicle = this.props.gasVehicle[0];
+      var hybridVehicle = this.props.hybridVehicle[0];
+      var gasRange = gasVehicle.range.gas;
+      var hybridGasRange = hybridVehicle.range.gas;
+      var hybridElectricRange = hybridVehicle.range.electric;
+      var chart = c3.generate({
+        bindto: '#RangeGraph',
+        data: {
+          columns: [
+              ['Gas', gasRange],
+              ['HybridGas', hybridGasRange],
+              ['HybridElectric', hybridElectricRange],
+          ],
+          types: {
+              Gas: 'bar',
+              HybridGas: 'bar',
+              HybridElectric: 'bar',
+          },
+          colors: {
+            Gas: '#4F4F4F',
+            HybridGas: '#4FA0CD',
+            HybridElectric: '#4FA9FF',
+          },
+          transition: {
+            duration: 2000
+          }
+        },
+        axis: {
+          rotated: true,
+          x: {show:true},
+          y: {show:true}
+        },
+        tooltip: {
+          format: {
+            title: function () { return 'Vehicle Range (Miles)'; }
+          }
+        }
+      }); //end of CHART
+    } //end IF
+    else if (this.props.hybridVehicle[0] === undefined &&
+      this.props.electricVehicle[0] === undefined) {
+      return null;
+    }
   }, //end RENDERCHART
 
   render: function() {
-    this.renderChart();
-
-        // setTimeout(function () {
-        //   chart.load({
-        //     columns: [
-        //       ['Gas', gasRange],
-        //       ['Hybrid', 0],
-        //       ['Electric', 0]
-        //     ]
-        //   })
-        // }, 600);
-        //   setTimeout(function () {
-        //   chart.load({
-        //     columns: [
-        //       ['Gas', gasRange],
-        //       ['Hybrid', hybridRange],
-        //       ['Electric', 0]
-        //     ]
-        //   })
-        // }, 900);
-        //     setTimeout(function () {
-        //   chart.load({
-        //     columns: [
-        //       ['Gas', gasRange],
-        //       ['Hybrid', hybridRange],
-        //       ['Electric', electricRange]
-        //     ]
-        //   })
-        // }, 1200);
-    return (
-      <div className="RangeGraph">
-        <h3>Range Graph</h3>
-        <div id="RangeGraph"></div>
-      </div>
-    )
+    if (this.renderChart() !== null){
+      this.renderChart()
+      return (
+        <div className="RangeGraph">
+          <h3>Range Graph</h3>
+          <div id="RangeGraph"></div>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className="RangeGraph">
+          <h3>Range Graph</h3>
+          <p>No range comparisons avilable</p>
+        </div>
+      )
+    }
   }
 });
 
