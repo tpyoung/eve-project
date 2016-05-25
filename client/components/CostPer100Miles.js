412f451de5d49fef7a29c401f154cb7c06d424fa
Laura <lg3295@gmail.com>
2016-05-25 10:18:58 -1000

    tests that props are sent

--- client/components/CostPer100Miles.js
+++ client/components/CostPer100Miles.js
@@ -2,37 +2,41 @@
 
 import React from 'react';
 
-const CostPerMileGraph = React.createClass({
+const costPer100Miles = React.createClass({
   render() {
+    console.log('this.props',this.props);
+    return <h1>HALLOOOOO!</h1>
 
-    var electricPer100 = this.props.energyPer100Miles.electric * this.props.state.electricCost;
-    var gasPer100 = this.props.energyPer100Miles.gas * this.props.state.gasCost;
+    // var electricPer100 = this.props.energyPer100Miles.electric * this.props.state.electricCost;
+    // var gasPer100 = this.props.energyPer100Miles.gas * this.props.state.gasCost;
 
-    if (electricPer100 !== null && gasPer100 !== null) {
-      return (
-        <div className='costPer100Miles'>
-          <p>Cost Per 100 Miles</p>
-          <p>Electric: { electricPer100 }</p>
-          <p>Gas: { gasPer100 }</p>
-        </div>
-      )
-    }
-    else if (gasPer100 !== null) {
-      return (
-        <div className='costPer100Miles'>
-          <p>Cost Per 100 Miles</p>
-          <p>Gas: { gasPer100 }</p>
-        </div>
-      )
-    }
-    else if (electricPer100 !== null) {
-      return (
-        <div className='costPer100Miles'>
-          <p>Cost Per 100 Miles</p>
-          <p>Electric: { electricPer100 }</p>
-        </div>
-      )
-    }
+    // if (electricPer100 !== null && gasPer100 !== null) {
+    //   return (
+    //     <div className='costPer100Miles'>
+    //       <p>Cost Per 100 Miles</p>
+    //       <p>Electric: { electricPer100 }</p>
+    //       <p>Gas: { gasPer100 }</p>
+    //     </div>
+    //   )
+    // }
+    // else if (gasPer100 !== null) {
+    //   return (
+    //     <div className='costPer100Miles'>
+    //       <p>Cost Per 100 Miles</p>
+    //       <p>Gas: { gasPer100 }</p>
+    //     </div>
+    //   )
+    // }
+    // else if (electricPer100 !== null) {
+    //   return (
+    //     <div className='costPer100Miles'>
+    //       <p>Cost Per 100 Miles</p>
+    //       <p>Electric: { electricPer100 }</p>
+    //     </div>
+    //   )
+    // }
   }
 });
+
+export default costPer100Miles;
 
