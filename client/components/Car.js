9c6662843850380adba433207893b4c74576cf20
maliaoreta <maliaoreta@gmail.com>
2016-05-25 19:15:36 -1000

    basic styling for car cards

--- client/components/Car.js
+++ client/components/Car.js
@@ -22,46 +22,48 @@ const Car = React.createClass({
     this.props.getCurrCar('previous electric');
   },
   render: function () {
-    let header;
     let onClickNext;
     let onClickPrev;
     if (this.props.vehicleInfo) {
       switch (this.props.vehicleInfo.power) {
         case 'Gas':
-          header = 'Gas';
           onClickNext = this.nextGasCar;
           onClickPrev = this.prevGasCar;
           break;
         case 'Plug-In Hybrid':
-          header = 'Plug-In Hybrid';
           onClickNext = this.nextHybridCar;
           onClickPrev = this.prevHybridCar;
           break;
         case 'Electric':
-          header = 'Electric';
           onClickNext = this.nextElectricCar;
           onClickPrev = this.prevElectricCar;
           break;
       }
     }
-    console.log('this.props.vehicleInfo.photoLink: ', this.props.vehicleInfo.photoLink);
     return (
       <div className="carCards">
         {this.props.vehicleInfo && <div className="car">
-          <h3>{header}</h3>
-          <img src={this.props.vehicleInfo.photoLink} />
-          <div>Model</div> 
-          {`${this.props.vehicleInfo.manufacturer} ${this.props.vehicleInfo.model}`}<br/>
-          MSRP: {this.props.vehicleInfo.msrp}<br/>
-          {(this.props.vehicleInfo.power==='Gas') && <div>MPG: {this.props.vehicleInfo.mpgmpge.gas}</div>}
-          {(this.props.vehicleInfo.power==='Plug-In Hybrid') &&
-            <div>MPGe: {this.props.vehicleInfo.mpgmpge.electric}<br/>
-            MPG: {this.props.vehicleInfo.mpgmpge.gas}</div>}
-          {(this.props.vehicleInfo.power==='Electric') && <div>MPGe: {this.props.vehicleInfo.mpgmpge.electric}</div>}
+          <img className="carPhotos" src={this.props.vehicleInfo.photoLink} />
+          <div className="carInfoContainer">
+            <div className="modelLabel">Model</div>
+            <div className="modelInfo">{`${this.props.vehicleInfo.manufacturer} ${this.props.vehicleInfo.model}`}</div>
+            <div className="carLabels">MSRP</div>
+            {(this.props.vehicleInfo.power==='Gas') && <div className="carLabels">MPG</div>}
+            {(this.props.vehicleInfo.power==='Plug-In Hybrid') && <div className="carLabels">MPGe</div>}
+            {(this.props.vehicleInfo.power==='Plug-In Hybrid') && <div className="carLabels">MPG</div>}
+            {(this.props.vehicleInfo.power==='Electric') && <div className="carLabels">MPGe</div>}
+            {(this.props.vehicleInfo.power==='Gas' || this.props.vehicleInfo.power==='Electric') && <div className="carInfo">{this.props.vehicleInfo.msrp}</div>}
+            {(this.props.vehicleInfo.power==='Plug-In Hybrid') && <div className="hybridCarInfo">{this.props.vehicleInfo.msrp}</div>}
+            {(this.props.vehicleInfo.power==='Gas') && <div className="carInfo">{this.props.vehicleInfo.mpgmpge.gas}</div>}
+            {(this.props.vehicleInfo.power==='Plug-In Hybrid') && <div className="hybridCarInfo">{this.props.vehicleInfo.mpgmpge.electric}</div>}
+            {(this.props.vehicleInfo.power==='Plug-In Hybrid') && <div className="hybridCarInfo">{this.props.vehicleInfo.mpgmpge.gas}</div>}
+            {(this.props.vehicleInfo.power==='Electric') && <div className="carInfo">{this.props.vehicleInfo.mpgmpge.electric}</div>}
+          </div>
+          <div className="stateIncentiveHeader">State Incentives</div>
           <StateIncentives stateInfo={this.props.stateInfo[0]} vehicleInfo={this.props.vehicleInfo} /> 
         </div>}
-        <button onClick={onClickPrev}>left</button>
-        <button onClick={onClickNext}>right</button>
+        <button className="leftButton" onClick={onClickPrev}><img src="http://icons.iconarchive.com/icons/iconsmind/outline/32/Arrow-Left-icon.png" /></button>
+        <button className="rightButton" onClick={onClickNext}><img src="http://icons.iconarchive.com/icons/iconsmind/outline/32/Arrow-Right-icon.png" /></button>
       </div>
     )
   }
